fcad1f32966156a1bfef537acf8d8d555dfe51f9
Shivam Shrey <31056227+shivamshrey@users.noreply.github.com>
2021-12-20 13:23:26 +0530

    Add all files

---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/CredentialForm.java
@@ -0,0 +1,42 @@
+package com.udacity.jwdnd.course1.cloudstorage.model;
+
+public class CredentialForm {
+
+    private String url;
+    private String userName;
+    private String password;
+    private String credentialId;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCredentialId() {
+        return credentialId;
+    }
+
+    public void setCredentialId(String credentialId) {
+        this.credentialId = credentialId;
+    }
+
+}
---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/NoteService.java
@@ -0,0 +1,41 @@
+package com.udacity.jwdnd.course1.cloudstorage.services;
+
+import com.udacity.jwdnd.course1.cloudstorage.mapper.NoteMapper;
+import com.udacity.jwdnd.course1.cloudstorage.mapper.UserMapper;
+import com.udacity.jwdnd.course1.cloudstorage.model.Note;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NoteService {
+
+    private UserMapper userMapper;
+    private NoteMapper noteMapper;
+
+    public NoteService(UserMapper userMapper, NoteMapper noteMapper) {
+        this.userMapper = userMapper;
+        this.noteMapper = noteMapper;
+    }
+
+    public void addNote(String title, String description, String userName) {
+        Integer userId = userMapper.getUser(userName).getUserId();
+        Note note = new Note(0, title, description, userId);
+        noteMapper.insert(note);
+    }
+
+    public void deleteNote(Integer noteId) {
+        noteMapper.deleteNote(noteId);
+    }
+
+    public Note[] getNoteListings(Integer userId) {
+        return noteMapper.getNotesForUser(userId);
+    }
+
+    public Note getNote(Integer noteId) {
+        return noteMapper.getNote(noteId);
+    }
+
+    public void updateNote(Integer noteId, String title, String description) {
+        noteMapper.updateNote(noteId, title, description);
+    }
+
+}
